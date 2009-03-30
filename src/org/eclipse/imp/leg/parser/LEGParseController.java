97a151c961c1158eb94d5dd6c469964ee48a3030
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-03-30 17:25:01 +0000

    Pulled implementation of getParser() and getLexer() up to SimpleLPGParseController, now that IParser and ILexer provide sufficient API for most needs. Removed local impl of getNodeLocator(), now that there's a single impl for all LPG-based parsers. Removed unnecessary override of initialize(). Accommodated change to IParseController.parse(): no longer takes the dummy/bogus argument "scanOnly".

--- src/org/eclipse/imp/leg/parser/LEGParseController.java
+++ src/org/eclipse/imp/leg/parser/LEGParseController.java
@@ -2,56 +2,22 @@ package org.eclipse.imp.leg.parser;
 
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.imp.services.ILanguageSyntaxProperties;
-import org.eclipse.imp.model.ISourceProject;
-import org.eclipse.imp.parser.ILexer;
-import org.eclipse.imp.parser.IMessageHandler;
-import org.eclipse.imp.parser.IParseController;
-import org.eclipse.imp.parser.IParser;
-import org.eclipse.imp.parser.ISourcePositionLocator;
-import org.eclipse.imp.parser.MessageHandlerAdapter;
-import org.eclipse.imp.parser.SimpleLPGParseController;
-
 import org.eclipse.imp.leg.Activator;
 import org.eclipse.imp.leg.parser.Ast.ASTNode;
+import org.eclipse.imp.model.ISourceProject;
+import org.eclipse.imp.parser.IMessageHandler;
+import org.eclipse.imp.parser.IParseController;
+import org.eclipse.imp.parser.MessageHandlerAdapter;
+import org.eclipse.imp.parser.SimpleLPGParseController;
+import org.eclipse.imp.services.ILanguageSyntaxProperties;
 
 /**
  * The LEG implementation of the IParseController IMP interface.
  */
 public class LEGParseController extends SimpleLPGParseController implements
         IParseController {
-    private LEGParser parser;
-
-    private LEGLexer lexer;
-
     public LEGParseController() {
         super(Activator.kLanguageID);
-    }
-
-    /**
-     * @param filePath
-     *            Absolute path of file
-     * @param project
-     *            Project that contains the file
-     * @param handler
-     *            A message handler to receive error messages (or any others)
-     *            from the parser
-     */
-    public void initialize(IPath filePath, ISourceProject project,
-            IMessageHandler handler) {
-        super.initialize(filePath, project, handler);
-    }
-
-    public IParser getParser() {
-        return parser;
-    }
-
-    public ILexer getLexer() {
-        return lexer;
-    }
-
-    public ISourcePositionLocator getNodeLocator() {
-        return new LEGSourcePositionLocator();
     }
 
     public ILanguageSyntaxProperties getSyntaxProperties() {
@@ -61,31 +27,26 @@ public class LEGParseController extends SimpleLPGParseController implements
     /**
      * setFilePath() should be called before calling this method.
      */
-    public Object parse(String contents, boolean scanOnly,
-            IProgressMonitor monitor) {
+    public Object parse(String contents, IProgressMonitor monitor) {
         PMMonitor my_monitor= new PMMonitor(monitor);
         char[] contentsArray= contents.toCharArray();
 
-        if (lexer == null) {
-            lexer= new LEGLexer();
+        if (fLexer == null) {
+            fLexer= new LEGLexer();
         }
-        lexer.reset(contentsArray, fFilePath.toPortableString());
+        fLexer.reset(contentsArray, fFilePath.toPortableString());
 
-        if (parser == null) {
-            parser= new LEGParser(lexer.getLexStream());
+        if (fParser == null) {
+            fParser= new LEGParser(fLexer.getILexStream());
         }
-        parser.reset(lexer.getLexStream());
-        parser.getParseStream().setMessageHandler(
-                new MessageHandlerAdapter(handler));
+        fParser.reset(fLexer.getILexStream());
+        fParser.getIPrsStream().setMessageHandler(new MessageHandlerAdapter(handler));
 
-        lexer.lexer(my_monitor, parser.getParseStream()); // Lex the stream to
-        // produce the token
-        // stream
+        fLexer.lexer(my_monitor, fParser.getIPrsStream()); // Lex the stream to produce the token stream
         if (my_monitor.isCancelled())
-            return fCurrentAst; // TODO fCurrentAst might (probably will) be
-        // inconsistent wrt the lex stream now
+            return fCurrentAst; // TODO fCurrentAst might (probably will) be inconsistent wrt the lex stream now
 
-        fCurrentAst= parser.parser(my_monitor, 0);
+        fCurrentAst= fParser.parser(my_monitor, 0);
 
         // SMS 18 Dec 2007:  functionDeclarationList is a LEG-specific type
         // not suitable for other languages; try replacing with ASTNode
@@ -93,7 +54,7 @@ public class LEGParseController extends SimpleLPGParseController implements
         //			parser.resolve((ASTNode) fCurrentAst);
         //		}
         if (fCurrentAst instanceof ASTNode) {
-            parser.resolve((ASTNode) fCurrentAst);
+            ((LEGParser) fParser).resolve((ASTNode) fCurrentAst);
         }
 
         cacheKeywordsOnce();
