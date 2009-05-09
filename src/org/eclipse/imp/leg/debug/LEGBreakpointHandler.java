eb0d29d346ee1e6bc29f95f3e8374a1a512e4904
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-05-09 20:24:17 +0000

    Factored out the language-independent code (which was 99% of it) into new base class JavaBreakpointHandler, which resides in the IMP runtime.

---
+++ src/org/eclipse/imp/leg/debug/LEGBreakpointHandler.java
@@ -0,0 +1,9 @@
+package org.eclipse.imp.leg.debug;
+
+import org.eclipse.imp.services.base.JavaBreakpointHandler;
+
+public class LEGBreakpointHandler extends JavaBreakpointHandler {
+    public LEGBreakpointHandler() {
+        super("leg");
+    }
+}
