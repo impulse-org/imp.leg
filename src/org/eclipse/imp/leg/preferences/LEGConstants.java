8f73184658d8097d94897ba4db5a5e8329a65a86
Robert M. Fuhrer <rmfuhrer@optonline.net>
2010-10-04 17:47:37 +0000

    Cosmetic changes from latest prefspecs compiler.

--- src/org/eclipse/imp/leg/preferences/LEGConstants.java
+++ src/org/eclipse/imp/leg/preferences/LEGConstants.java
@@ -1,3 +1,6 @@
+/******************************************/
+/* WARNING: GENERATED FILE - DO NOT EDIT! */
+/******************************************/
 package org.eclipse.imp.leg.preferences;
 
 /**
@@ -6,21 +9,11 @@ package org.eclipse.imp.leg.preferences;
  * The preferences service uses Strings as keys for preference values,
  * so Strings defined here are used here to designate preference fields.
  * These strings are generated automatically from a preferences specification.
- * Other constants may be defined here manually.
- *
  */
-
-
 public class LEGConstants {
-
 	public static final String P_USEDEFAULTINCLUDEPATH = "UseDefaultIncludePath";
-
 	public static final String P_INCLUDEPATHTOUSE = "IncludePathToUse";
-
 	public static final String P_SOURCEFILEEXTENSIONS = "SourceFileExtensions";
-
 	public static final String P_GENERATELOG = "GenerateLog";
-
 	public static final String P_MAXLOGENTRIES = "MaxLogEntries";
-
 }
